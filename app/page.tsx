d01c20402301f9fefe57cf32a27f686563c4e04b
tian <istiancz@gmail.com>
2024-07-12 17:09:16 +0800

    feat: add zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import { SubmitButton } from "@/components/Submit";
 import { neon } from "@neondatabase/serverless";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
+const schema = z.object({
+  email: z.string().email(),
+});
 export default async function Home() {
   async function create(formData: FormData) {
     "use server";
     const sql = neon(process.env.DATABASE_URL!);
-    const email = formData.get("email");
+    const validatedFields = schema.safeParse({
+      email: formData.get("email"),
+    });
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+      };
+    }
     const res = await sql(
       "INSERT INTO emails (email) VALUES ($1) RETURNING short_id",
-      [email],
+      [validatedFields.data.email],
     );
     const id: string = res[0].short_id;
 
