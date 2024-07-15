8d8c703e3405b7b098539a5ae3d8348abfb6ca14
tian <istiancz@gmail.com>
2024-07-15 20:03:22 +0800

    feat: add instructions

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { SubmitButton } from "@/components/Submit";
 import { neon } from "@neondatabase/serverless";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
@@ -28,6 +29,21 @@ export default async function Home() {
   }
   return (
     <main className="min-h-screen sm:p-24 flex flex-col items-center justify-center">
+      <Link
+        href="https://x.com/mws/status/1811816494529237248"
+        target="_blank"
+        className="text-2xl font-medium hover:underline"
+      >
+        YouTube removed your channel{"'"}s Business Email section.
+      </Link>
+      <h1 className="text-xl max-w-2xl mt-2">
+        Don{`'`}t worry. This site helps YouTubers manage their email and
+        <i className="font-medium"> avoid bots/scammers</i>.
+        Sponsors/advertisers can use it to find your email.
+      </h1>
+      <Link href="/ff92ee66" className="mb-6 text-lg">
+        demo email link: <span className="underline">mailof.me/ff92ee66</span>
+      </Link>
       <form className="w-full" action={create}>
         <div className="flex justify-center items-center text-lg">
           Email:
--- components/Submit.tsx
+++ components/Submit.tsx
@@ -11,7 +11,7 @@ export function SubmitButton() {
       disabled={pending}
       className="border block mx-auto rounded py-2 px-4 text-lg mt-6 bg-green-400 text-white disabled:opacity-50 disabled:cursor-not-allowed"
     >
-      {pending ? "Creating..." : "Create Link"}
+      {pending ? "Creating..." : "Create my link"}
     </button>
   );
 }
